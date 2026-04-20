abbee777ce293f8161a5e9e8880d62a8f916e275
Paul Frederiksen <paul@paulfrederiksen.com>
2026-04-20 09:52:37 -0700

    openclaw-cost-diff 0.1.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/openclaw-cost-diff.rb
+++ Formula/openclaw-cost-diff.rb
@@ -6,8 +6,8 @@ class OpenclawCostDiff < Formula
 
   desc "Compare OpenClaw token usage and API cost"
   homepage "https://github.com/pfrederiksen/openclaw-cost-diff"
-  url "https://github.com/pfrederiksen/openclaw-cost-diff/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "2bfb061ff8d23d3aa3540e0ffa4ec520d1a62a3239cd9b752e23eb197566cde4"
+  url "https://github.com/pfrederiksen/openclaw-cost-diff/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "52e08279432a42c6413b62c75471906915e42856593e4f5339675d8334bf6fa4"
   license "MIT"
 
   depends_on "python-setuptools" => :build
